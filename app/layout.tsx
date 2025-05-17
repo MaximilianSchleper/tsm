0881440f6a2338730b83b7fea81e7e291953f7d5
mslep <mx.schleper@gmail.com>
2025-05-17 14:32:03 +0200

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import "./globals.css";
+
+import React from "react";
+
+import { Providers } from "~/components/providers";
+import { TailwindIndicator } from "~/components/tailwind-indicator";
+import * as fonts from "~/lib/fonts";
+import { cn } from "~/lib/utils";
+
+export const metadata = {
+  title: "Next.js + TypeScript Starter",
+  description: "A starter template for Next.js and TypeScript",
+};
+
+const RootLayout: React.FCC = ({ children }) => {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn(Object.values(fonts).map((font) => font.variable))}
+    >
+      <body className="min-h-dvh scroll-smooth font-sans antialiased">
+        <Providers>{children}</Providers>
+
+        <TailwindIndicator />
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ components/providers.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import * as React from "react";
+
+import { ThemeProvider } from "next-themes";
+
+import type { ThemeProviderProps } from "next-themes";
+
+export const Providers: React.FCC<{
+  theme?: ThemeProviderProps;
+}> = ({ theme, children }) => {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+      {...theme}
+    >
+      {children}
+    </ThemeProvider>
+  );
+};
