0985f4369dcb9fa4142bc67044dc61bc3fa827a5
mslep <mx.schleper@gmail.com>
2025-05-26 22:25:58 +0200

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Analytics } from "@vercel/analytics/next";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Analytics />
       <body>
         {children}
       </body>
