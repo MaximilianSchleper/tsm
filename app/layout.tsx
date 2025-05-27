7fd88146f40434cd73ee91dd85816591cb581298
mslep <mx.schleper@gmail.com>
2025-05-27 15:21:38 +0200

    speedinsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import "./globals.css";
 import React from 'react';
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <Analytics />
+      <SpeedInsights />
       <body>
         {children}
       </body>
